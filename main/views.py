e51082a61a5e419a42025e6870b60376d6ae4b2e
Ashwin Singh <legend.ashwini07@gmail.com>
2019-06-23 15:00:13 +0530

    code analyzer setup

---
+++ main/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+import os
+import subprocess
+import json
+
+
+@csrf_exempt 
+# Create your views here.
+
+def index(request):
+    json_object = json.loads(request.body)
+    data  = json_object['data']['code']
+    f = open("javascript.js", "w")
+    f.write(data);
+    f.close()
+ 
+    shell_output =  subprocess.Popen('nodejsscan -f javascript.js', shell=True, stdout=subprocess.PIPE).stdout
+    shell_output =  shell_output.read();
+    print(os.system('nodejsscan -f javascript.js'));
+    print(os.system('jshint javascript.js'))
+    responseData = {
+        'message': shell_output.decode(),
+    }
+    return HttpResponse(json.dumps(responseData), content_type='application/json');
