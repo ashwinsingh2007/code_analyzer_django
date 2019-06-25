97fae2b0db0692b22f81bc3cd84a4a476cecce87
Ashwin Singh <legend.ashwini07@gmail.com>
2019-06-25 23:54:05 +0530

    for prod

--- main/views.py
+++ main/views.py
@@ -10,18 +10,18 @@ import json
 # Create your views here.
 
 def index(request):
-    json_object = json.loads(request.body)
-    data  = json_object['data']['code']
-    f = open("javascript.js", "w")
-    f.write(data);
-    f.close()
+    # json_object = json.loads(request.body)
+    # data  = json_object['data']['code']
+    # f = open("javascript.js", "w")
+    # f.write(data);
+    # f.close()
  
     shell_output =  subprocess.Popen('nodejsscan -f javascript.js', shell=True, stdout=subprocess.PIPE).stdout
     shell_output =  shell_output.read();
     print(os.system('nodejsscan -f javascript.js'));
     print(os.system('jshint javascript.js'))
-    responseData = {
-        'message': shell_output.decode(),
-    }
-    return HttpResponse(json.dumps(responseData), content_type='application/json');
-    # return HttpResponse('Hello Python');
+    # responseData = {
+    #     'message': shell_output.decode(),
+    # }
+    # return HttpResponse(json.dumps(responseData), content_type='application/json');
+    return HttpResponse(shell_output);
