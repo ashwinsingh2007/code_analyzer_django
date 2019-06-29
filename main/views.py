73acb500d34fe1dd476f1defeda41de7db516ef0
Ashwin Singh <legend.ashwini07@gmail.com>
2019-06-30 00:35:24 +0530

    finishing off in style

--- main/views.py
+++ main/views.py
@@ -3,11 +3,17 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 import os
 import subprocess
-import json
+# import json
+import simplejson as json
 
 
 @csrf_exempt 
 # Create your views here.
+
+# def console(cmd):
+#     p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+#     out, err = p.communicate()
+#     return (p.returncode, out, err)
 
 def index(request):
     json_object = json.loads(request.body)
@@ -16,12 +22,20 @@ def index(request):
     f.write(data);
     f.close()
  
-    shell_output =  subprocess.Popen('nodejsscan -f javascriptTest.js', shell=True, stdout=subprocess.PIPE).stdout
-    shell_output =  shell_output.read();
+    shell_output_security =  subprocess.Popen('nodejsscan -f javascriptTest.js -o output.json', shell=True, stdout=subprocess.PIPE);
+    shell_output_security.wait();
+    shell_output_lint =  subprocess.Popen('jshint javascriptTest.js', shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE);
+    shell_output_lint.wait();
+    f = open('output.json', "r")
+    v = json.loads(f.read());
     # print(os.system('nodejsscan -f javascript.js'));
-    # print(os.system('jshint javascript.js'))
+    # print('shell_output_lint--', shell_output_lint.communicate())
+    output, err = shell_output_lint.communicate()
+    rc = shell_output_lint.returncode
+    print('--output---', output);
     responseData = {
-        'message': shell_output.decode(),
+        'message': v,
+        'lint': output,
     }
     return HttpResponse(json.dumps(responseData), content_type='application/json');
     # return HttpResponse(shell_output);
